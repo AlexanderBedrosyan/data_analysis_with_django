00f80421fda55e7c0e670ed5dc5ab8fecb254b98
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-04-15 00:16:44 +0300

    Update urls.

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+from .views import Home
+
 from django.conf import settings
 
-urlpatterns = []
+urlpatterns = [
+    path('', Home.as_view(), name='home'),
+]
