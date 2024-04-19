5dbde76e91992f7f32b9569e5b3bcd8538b5f6e3
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-04-19 23:15:55 +0300

    Add path to favicon.ico

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 from .views import Home
+from django.views.generic.base import RedirectView
 
 from django.conf import settings
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('favicon.ico', RedirectView.as_view(url=''), name='favicon'),
 ]
