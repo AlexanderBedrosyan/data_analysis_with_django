e90a307833e1ca872eca3e70b04e0128484def0b
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-04-22 10:53:56 +0300

    Add static files, update settings and urls.

--- data_analysis_with_django/settings.py
+++ data_analysis_with_django/settings.py
@@ -120,15 +120,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
-# STATIC_URL = 'static/'
+# STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
-#     BASE_DIR
+#     BASE_DIR / 'staticfiles'
 # ]
 #
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
